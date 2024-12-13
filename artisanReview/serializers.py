5acb5d8649ccbaa3c5e10cbfc6237a8eb9b4e659
EkeHanson <ekenehanson@gmail.com>
2024-12-13 10:56:38 +0100

    Reviewer Nmae now showing

--- artisanReview/serializers.py
+++ artisanReview/serializers.py
@@ -3,13 +3,21 @@ from .models import ArtisanReview
 from users.models import CustomUser
 
 class ArtisanReviewSerializer(serializers.ModelSerializer):
-    reviewer_name = serializers.SerializerMethodField()
+    reviewer_name_display = serializers.SerializerMethodField(read_only=True)
+    reviewer_name = serializers.SlugRelatedField(
+        queryset=CustomUser.objects.all(),
+        slug_field='unique_id',  # Use unique_id for lookups
+        write_only=True
+    )
 
     class Meta:
         model = ArtisanReview
         fields = '__all__'
+        extra_kwargs = {
+            'reviewer_name': {'write_only': True}  # Hide from GET responses
+        }
 
-    def get_reviewer_name(self, obj):
+    def get_reviewer_name_display(self, obj):
         """
         Returns the first_name and last_name of the CustomUser linked to reviewer_name.
         """
