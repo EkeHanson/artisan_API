10464196a9a9c325fee3e1fbe3267bd9be1afa3f
YourGitHubUsername <EkeHanson>
2025-02-24 11:23:22 +0100

    ok

--- users/models.py
+++ users/models.py
@@ -77,6 +77,8 @@ class CustomUser(AbstractBaseUser):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
+    is_suspended = models.BooleanField(default=False)
+    is_subscribed = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
 
     is_superuser = models.BooleanField(default=False)
